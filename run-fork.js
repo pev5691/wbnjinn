cd487706672d7d48e5a9f8a0e7a170ecda7d72d4
pev5691 <pev5691@yandex.ru>
2020-08-25 08:21:42 +0300

    Подготовка к клонированию

---
+++ run-fork.js
@@ -0,0 +1,10 @@
+global.FORK_MODE = 1; //set to 1 for fork
+global.NETWORK = "WBN-MAIN"; //max 10 chars
+global.START_NETWORK_DATE = 1598332785294; //formula of it value is (new Date(2019, 8, 20, 17, 0, 0, 0))-0;
+global.CONSENSUS_PERIOD_TIME = 3000; //ms
+
+global.FORK_IP_LIST = [
+//    {"ip":"185.26.121.248","port":50005},   // hostland
+    {"ip":"194.67.221.153","port":34734},   // ihor totoha
+//    {"ip":"188.227.85.27","port":50005},    // Server Space
+    {"ip":"194.147.78.188","port":34734}];  // Vdska 194.147.78.188
